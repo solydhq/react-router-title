2060c1d0194daf041af9713441497fd60d21cbcb
Paul Tiedtke <PaulTiedtke@web.de>
2019-10-03 04:02:28 +0200

    Add typescript declaration file

---
+++ dist/index.d.ts
@@ -0,0 +1,23 @@
+import { match } from "react-router";
+import { RouteConfig } from "react-router-config";
+import { Location } from "history";
+interface TitleObject {
+    title: string;
+    titles: string[];
+    params: match["params"];
+}
+declare type Routes = RouteConfigExtended[] | {
+    [name: string]: RouteConfigExtended;
+};
+export interface RouteConfigExtended extends Omit<RouteConfig, "routes"> {
+    title: string;
+    routes?: Routes;
+}
+interface RouterTitleProps {
+    routesConfig: RouteConfigExtended;
+    callback?: (titleObject: TitleObject, location: Location) => Promise<string> | string;
+    pageTitle?: string;
+    divider?: string;
+}
+declare const RouterTitle: ({ pageTitle, routesConfig, callback, divider, }: RouterTitleProps) => any;
+export default RouterTitle;
